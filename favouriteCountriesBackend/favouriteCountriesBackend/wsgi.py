8b06126b969a4e7bf0f0062a27d2e9ba5c9b2629
Daniel W <deniel98045@gmail.com>
2024-06-05 15:13:32 +0200

    solving 500

--- favouriteCountriesBackend/favouriteCountriesBackend/wsgi.py
+++ favouriteCountriesBackend/favouriteCountriesBackend/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'favouriteCountriesBackend.settings')
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
